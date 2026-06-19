d7020cd62316174fb806b5d4bbcc9af6b4b33ec8
TaeHeeKim <eksrnsdhkdwk@gmail.com>
2026-06-20 02:22:31 +0900

    커밋

--- chat.py
+++ chat.py
@@ -25,7 +25,7 @@ if user_question := st.chat_input(placeholder="소득세에 관련된 궁금한 
     st.session_state.message_list.append({"role": "user", "content": user_question})
 
     with st.spinner("답변을 생성하는 중입니다."):
-        ai_message = get_ai_message(user_question)
+        ai_response = get_ai_message(user_question)
         with st.chat_message("ai"):
-            st.write(ai_message)
-        st.session_state.message_list.append({"role": "ai", "content": user_question})
+            ai_message = st.write_stream(ai_response)
+            st.session_state.message_list.append({"role": "ai", "content": ai_message})
